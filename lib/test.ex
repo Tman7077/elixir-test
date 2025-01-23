d85f18fc6b26386de5ebb6a866aadf8190f27e1d
Tyler Bartle <tman.bartleboy1@gmail.com>
2025-01-23 10:32:16 -0700

    Basic setup

---
+++ lib/test.ex
@@ -0,0 +1,18 @@
+defmodule Elixir_test do
+  @moduledoc """
+  Documentation for `Test`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixir_test.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
